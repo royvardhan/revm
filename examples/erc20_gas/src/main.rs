c4d5b832ac5ed9b283c8fcb3f179759e64d8690b
royvardhan <royvardhan.official@gmail.com>
2024-11-18 09:44:07 +0530

    feat: extend main

--- examples/erc20_gas/src/main.rs
+++ examples/erc20_gas/src/main.rs
@@ -2,13 +2,13 @@ use alloy_provider::{network::Ethereum, ProviderBuilder, RootProvider};
 use alloy_sol_types::{abi::token, sol, SolCall, SolValue};
 use alloy_transport_http::Http;
 use anyhow::{anyhow, Result};
-use database::{AlloyDB, CacheDB};
+use database::{AlloyDB, BlockId, CacheDB};
 use reqwest::Client;
 use revm::{
     database_interface::WrapDatabaseAsync,
     handler::mainnet::validate_tx_against_account,
     primitives::{address, keccak256, Address, Bytes, TxKind, U256},
-    specification::hardfork::Spec,
+    specification::hardfork::{LatestSpec, Spec},
     state::{AccountInfo, EvmStorageSlot},
     wiring::{
         result::{EVMError, ExecutionResult, InvalidTransaction, Output},
@@ -17,6 +17,9 @@ use revm::{
     Database, Evm, EvmHandler, EvmWiring,
 };
 use std::sync::Arc;
+
+type AlloyCacheDB =
+    CacheDB<WrapDatabaseAsync<AlloyDB<Http<Client>, Ethereum, RootProvider<Http<Client>>>>>;
 
 // Define ERC20 interface
 sol! {
@@ -203,6 +206,122 @@ pub fn erc20_gas_handler_register<'a, EvmWiringT: EvmWiring, SPEC: Spec>(
     });
 }
 
+fn balance_of(token: Address, address: Address, alloy_db: &mut AlloyCacheDB) -> Result<U256> {
+    sol! {
+        function balanceOf(address account) public returns (uint256);
+    }
+
+    let encoded = balanceOfCall { account: address }.abi_encode();
+
+    let mut evm = Evm::<EthereumWiring<&mut AlloyCacheDB, ()>>::builder()
+        .with_db(alloy_db)
+        .with_default_ext_ctx()
+        .modify_tx_env(|tx| {
+            // 0x1 because calling USDC proxy from zero address fails
+            tx.caller = address!("0000000000000000000000000000000000000001");
+            tx.transact_to = TxKind::Call(token);
+            tx.data = encoded.into();
+            tx.value = U256::from(0);
+        })
+        .build();
+
+    let ref_tx = evm.transact().unwrap();
+    let result = ref_tx.result;
+
+    let value = match result {
+        ExecutionResult::Success {
+            output: Output::Call(value),
+            ..
+        } => value,
+        result => return Err(anyhow!("'balanceOf' execution failed: {result:?}")),
+    };
+
+    let balance = <U256>::abi_decode(&value, false)?;
+
+    Ok(balance)
+}
+
+fn transfer(
+    from: Address,
+    to: Address,
+    amount: U256,
+    token: Address,
+    cache_db: &mut AlloyCacheDB,
+) -> Result<()> {
+    sol! {
+        function transfer(address to, uint amount) external returns (bool);
+    }
+
+    let encoded = transferCall { to, amount }.abi_encode();
+
+    let mut evm = Evm::<EthereumWiring<&mut AlloyCacheDB, ()>>::builder()
+        .with_db(cache_db)
+        .with_default_ext_ctx()
+        .modify_tx_env(|tx| {
+            tx.caller = from;
+            tx.transact_to = TxKind::Call(token);
+            tx.data = encoded.into();
+            tx.value = U256::from(0);
+        })
+        .append_handler_register(
+            erc20_gas_handler_register::<EthereumWiring<&mut AlloyCacheDB, ()>, LatestSpec>,
+        )
+        .build();
+
+    let ref_tx = evm.transact_commit().unwrap();
+    let success: bool = match ref_tx {
+        ExecutionResult::Success {
+            output: Output::Call(value),
+            ..
+        } => <bool>::abi_decode(&value, false)?,
+        result => return Err(anyhow!("'transfer' execution failed: {result:?}")),
+    };
+
+    if !success {
+        return Err(anyhow!("'transfer' failed"));
+    }
+
+    Ok(())
+}
+
 fn main() -> Result<()> {
+    let rpc_url = "https://mainnet.infura.io/v3/c60b0bb42f8a4c6481ecd229eddaca27".parse()?;
+    let client = ProviderBuilder::new().on_http(rpc_url);
+    let alloy = WrapDatabaseAsync::new(AlloyDB::new(client, BlockId::latest())).unwrap();
+    let mut cache_db = CacheDB::new(alloy);
+
+    // Random empty account: From
+    let account = address!("18B06aaF27d44B756FCF16Ca20C1f183EB49111f");
+    // Random empty account: To
+    let account_to = address!("0x21a4B6F62E51e59274b6Be1705c7c68781B87C77");
+
+    let usdc = address!("a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48");
+
+    // USDC has 6 decimals
+    let hundred_tokens = U256::from(100_000_000_000_000_000u128);
+
+    let balance_slot = keccak256((account, U256::from(3)).abi_encode()).into();
+
+    cache_db.insert_account_storage(usdc, balance_slot, hundred_tokens)?;
+    cache_db.insert_account_info(
+        account,
+        AccountInfo {
+            nonce: 0,
+            balance: hundred_tokens,
+            code_hash: keccak256(Bytes::new()),
+            code: None,
+        },
+    );
+
+    let balance_before = balance_of(usdc, account, &mut cache_db)?;
+
+    // Transfer 100 tokens from account to account_to
+    transfer(account, account_to, hundred_tokens, usdc, &mut cache_db)?;
+
+    let balance_after = balance_of(usdc, account, &mut cache_db)?;
+
+    println!("Balance before: {balance_before}");
+    println!("Balance after: {balance_after}");
+
     Ok(())
 }
